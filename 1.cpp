d1788c8c8b2c7e38b0885af8a5a3f693f4ec3a05
Vedhanth <gvedhanth123@outlook.com>
2021-03-15 20:26:15 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,37 @@
+
+// #include <stdio.h> -- c example
+
+#include <iostream> // write .h in turbo c++
+using namespace std; // leave it in turbo c++
+
+// create an object student
+int main()
+{
+
+    // blueprint of a student
+    class Student
+    {
+        int pin;
+        string name;
+
+        // functions can be stored in a class
+        // read(), studying(),
+        void print_pin()
+        {
+            // printing a pin
+            // printf()
+            cout << pin;
+
+            // scanf()
+            // cin >> pin;
+        }
+
+        // creating an object of student class
+        
+    };
+
+
+
+
+}
+
